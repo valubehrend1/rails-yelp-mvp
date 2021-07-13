50147a4ea4e6916be82de8a04aac2bbef3f19ee0
Valeria Behrend <valu-sp@hotmail.com>
2021-07-13 20:14:09 +0200

    all validations ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,5 @@ class Restaurant < ApplicationRecord
     restaurant_categories = ["chinese", "italian", "japanese", "french", "belgian"]
 
     validates :name, :address, :category, presence: true
-    validates :rating, length: { in: 1..5 }
     validates_inclusion_of :category, in: restaurant_categories
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :content, :rating presence: true
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates_inclusion_of :rating, :in => 0..5
 end
