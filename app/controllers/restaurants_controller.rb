edc31259cd5dff7c1312c8793fb234f9d9de55dd
Valeria Behrend <valu-sp@hotmail.com>
2021-07-13 21:24:46 +0200

    views and controller basic actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+    def index
+        @restaurants = Restaurant.all
+    end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.new
+        @restaurant.save
+        redirect_to restaurant_path
+    end
+
+    def show
+        @restaurant = Restaurant.find(params[:id])
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :address)
+    end
 end
